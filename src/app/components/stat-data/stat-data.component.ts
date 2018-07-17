9f69a1ba3d4f335fde474ed69e58ad8210472592
Lukas Naruševičius <lukas@tsolutions.io>
2018-07-17 19:21:25 +0000

    Merged PR 83: Storage size slider in settings menu
    Related work items: #111

--- src/app/components/stat-data/stat-data.component.ts
+++ src/app/components/stat-data/stat-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnInit } from "@angular/core";
+import { UtilsService } from "../../providers/utils.service";
 
 @Component({
   selector: "stat-data",
@@ -11,33 +12,17 @@ export class StatDataComponent implements OnInit, OnChanges {
   units: string;
   bytesTransformed: number;
 
-  constructor() {}
+  constructor(private utilsService: UtilsService) {}
 
   ngOnInit() {
-    this.transformDataAndUnits()
+    const transformedBytes = this.utilsService.transformDataAndUnits(this.bytes);
+    this.bytesTransformed = transformedBytes.bytes;
+    this.units = transformedBytes.units;
   }
 
   ngOnChanges() {
-    this.transformDataAndUnits()
-  }
-
-  transformDataAndUnits() {
-    const B = 1
-    const kB = 1000 * B
-    const MB = 1000 * kB
-    const TB = 1000 * MB
-    if (this.bytes <= kB) {
-      this.units = "B"
-      this.bytesTransformed = this.bytes
-    } else if (this.bytes < MB) {
-      this.units = "kB"
-      this.bytesTransformed = Math.round(this.bytes / kB)
-    } else if (this.bytes < TB) {
-      this.units = "MB"
-      this.bytesTransformed = Math.round(this.bytes / MB)
-    } else {
-      this.bytesTransformed = Math.round(this.bytes / TB)
-      this.units = "TB"
-    }
+    const transformedBytes = this.utilsService.transformDataAndUnits(this.bytes);
+    this.bytesTransformed = transformedBytes.bytes;
+    this.units = transformedBytes.units;
   }
 }
